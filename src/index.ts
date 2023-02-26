b170f9cc1cc70304102d3aec14c05d8210d3552d
thewilley <thewilley25@gmail.com>
2023-02-26 23:36:38 +0100

    feat: added working parsing for all attributes (as of now)

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,178 @@
 //Puppeteer library
-const pt = require('puppeteer')
+const pt = require('puppeteer');
+var browserInstance: Page;
 
 // Bid is a bid on an auction
 interface Bid {
     bidder: string;
     bid: number;
-    time: number;
+    time: string;
 }
 
-// BidInfo is the information that is available on the auction page
-interface BidInfo {
+// AuctionInfo is the information that is available on the auction page
+interface AuctionInfo {
     // Acution and seller information
     sellerName: string;
-    isauction: boolean;
-    acutionName: string;
-    auctionStarted: boolean;
+    isAuction: boolean;
+    aucutionName: string;
     auctionEnded: boolean;
 
     // Bid information
     allBids: Bid[];
     highestBid: number;
+    latestBid: Bid
     numberOfBids: number;
 
     // Time information
     timeLeft: number;
     endTime: number;
-    latestBidTime: number;
-    latestBidder: string;
 
     // Buy now information
     buyNowPrice: number;
     buyNowAvailable: boolean;
 
     // Other information
-    shippingPrice: number;
     description: string;
     images: string[];
 }
 
-async function selectorId() {
+/**
+ * Helper functions for puppeteer
+ * @returns A helper object with functions that can be used to get information from the auction page
+ */
+function puppeteerHelper() {
+    return {
+        getSelectorText: async (selector: string, callback?: (_result_: string) => unknown) => {
+            // screenshot
+            await browserInstance.screenshot({ path: 'example.png' });
+
+            try {
+                const _result_ = await browserInstance.evaluate((selector: string) => {
+                    var element;
+                    element = document.querySelector(selector);
+
+                    return (element as HTMLElement).innerText;
+                }, selector);
+
+                // Check if callback is defined and return result
+                if (callback)
+                    return callback(_result_)
+                else
+                    return _result_
+            } catch (e) {
+                console.log("Could not get selector text")
+            }
+
+        },
+        getAllBids: async (callback?: (_result_: Bid[]) => unknown) => {
+            // Open bid history
+            await browserInstance.click('.bid-details-bids-title > span > a')
+            await browserInstance.waitForSelector(".bid-details-bids-title > span > a")
+
+            // Wait for bid history to load and get all bids
+            const _result_ = await browserInstance.evaluate(() => {
+                var bids: Bid[] = [];
+                var bidElements = document.querySelectorAll('.table-fixed > tbody > tr');
+                bidElements.forEach((element: HTMLElement) => {
+                    var bidder = (element.children[0] as HTMLElement).innerText;
+                    var bid = (element.children[1] as HTMLElement).innerText;
+                    var time = (element.children[3] as HTMLElement).innerText;
+
+                    bids.push({
+                        bidder: bidder,
+                        bid: parseFloat(bid.replace("kr", "")),
+                        time: time
+                    })
+                });
+
+                return bids;
+            });
+
+            // Check if callback is defined and return result
+            if (callback)
+                return callback(_result_)
+            else
+                return _result_
+        },
+        async getAllImages() {
+            const _result_ = await browserInstance.evaluate(() => {
+                var images: string[] = [];
+                var imageElements = document.querySelectorAll('.image-gallery-item__image');
+                imageElements.forEach((element: HTMLImageElement) => {
+                    images.push(element.src);
+                });
+
+                return images;
+            })
+
+            return _result_
+        }
+    }
+}
+
+/**
+ * Setup the browser instance
+ */
+async function setup() {
+    // Check if page url contains "item" and exit if not
+    if (!browserInstance.url().includes("item")) {
+        console.log("Not an auction page (page might have been removed)")
+        process.exit(0)
+    }
+
+    // Attempt to accept cookies
+    try {
+        await browserInstance.waitForSelector("#qc-cmp2-ui > div.qc-cmp2-footer.qc-cmp2-footer-overlay.qc-cmp2-footer-scrolled > div > button.css-14ubilm")
+        await browserInstance.click("#qc-cmp2-ui > div.qc-cmp2-footer.qc-cmp2-footer-overlay.qc-cmp2-footer-scrolled > div > button.css-14ubilm")
+    } catch (e) {
+        console.log("Could not click accept cookies")
+    }
+}
+
+/**
+ * Start browser instance
+ * @param auction_url The url to the auction
+ */
+async function startBrowser(auction_url: string) {
     //launch browser in headless mode
     const browser = await pt.launch()
     //browser new page
     const page = await browser.newPage();
     //launch URL
-    await page.goto('https://www.tradera.com/item/343397/584081849/super-smash-bros-nytt-nintendo-wii-u-wii-u')
-    //identify element with id
-    const name = await page.evaluate(() => {
-        const element = document.querySelector('.bid-details-amount');
-        return (element as HTMLElement).innerText.replace(/\D/g, '');
-    });
-    // Log name innerText
-    console.log(name)
-    //wait for sometime
-    await page.waitForTimeout(4000)
-    //browser close
-    await browser.close()
+    await page.goto(auction_url)
+    // Set browser instance
+    browserInstance = page;
+
+    // Wait for setup to finish
+    await setup();
 }
-selectorId()
+
+/**
+ * Get auction information
+ */
+async function getAuctionInfo() {
+    await startBrowser("https://www.tradera.com/item/344630/583933118/miitopia-nintendo-switch-")
+
+    // TODO: Make all selectors use jsselector from chromiun dev tools instead of css selectors (they are more reliable)
+    const auctionInfo: AuctionInfo = {
+        sellerName: await puppeteerHelper().getSelectorText('.seller-alias'),
+        isAuction: await puppeteerHelper().getSelectorText('.bid-details-bids-title', (result) => { return result.includes("Bud") ? true : false; }),
+        aucutionName: await puppeteerHelper().getSelectorText('#view-item-main'),
+        auctionEnded: await puppeteerHelper().getSelectorText('.my-auto > .heading-london', (result) => { return result.includes("Avslutad") ? true : false; }),
+        allBids: await puppeteerHelper().getAllBids(),
+        numberOfBids: (await puppeteerHelper().getAllBids()).length,
+        highestBid: await puppeteerHelper().getSelectorText(".bid-details-amount > span > span", (result) => { return Number(result.replace(/[^0-9.]/g, '')); }),
+        timeLeft: await puppeteerHelper().getSelectorText('#collapsed_auction_details > div > aside > div.separators_sm-separator__lOmzL.pb-md-2.mb-md-2 > section.bid-details.d-flex.flex-md-column.justify-content-between.py-1.pt-md-0.pb-md-2 > div.d-flex.flex-column.flex-md-row.justify-content-between.mb-md-2.text-center > div.my-auto > p > span', (result) => { return result != "Avslutad" ? result : 0; }),
+        latestBid: await puppeteerHelper().getAllBids((result) => { return result[0]; }),
+        buyNowPrice: await puppeteerHelper().getSelectorText("button.btn-md:nth-child(3)", (result) => { return Number(result.replace(/[^0-9.]/g, '')); }),
+        buyNowAvailable: await puppeteerHelper().getSelectorText("button.btn-md:nth-child(3)", (result) => { return result == undefined ? false : true; }),
+        description: await puppeteerHelper().getSelectorText(".overflow-hidden.text-break.position-relative"),
+        images: await puppeteerHelper().getAllImages(),
+        endTime: await puppeteerHelper().getSelectorText("#collapsed_auction_details > div > aside > div.separators_sm-separator__lOmzL.pb-md-2.mb-md-2 > section.bid-details.d-flex.flex-md-column.justify-content-between.py-1.pt-md-0.pb-md-2 > div.d-flex.flex-column.flex-md-row.justify-content-between.mb-md-2.text-center > div.mb-1.mb-md-0 > p", (result) => { return result.replace("Avslutas ", "") })
+    }
+
+    console.log(auctionInfo);
+    process.exit(0)
+}
+
+getAuctionInfo()
