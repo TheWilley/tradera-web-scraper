119ca9dcff27ebaffd5b7ba1988b215d7ce78124
thewilley <thewilley25@gmail.com>
2023-02-28 20:52:24 +0100

    perf: part 2, changed method to get attribute in the puppeteer instance, rather than in the callback function (BROKEN STATE)

--- src/tradera-web-scraper.ts
+++ src/tradera-web-scraper.ts
@@ -61,20 +61,21 @@ type PageInfo = AuctionInfo | ProductInfo;
  */
 function puppeteerHelper() {
     return {
-        getSelector: async (selector: string, callback: (_result_: puppeteer.JSHandle) => unknown) => {
+        getSelectorText: async (selector: string, property: keyof Element, callback?: (result: unknown) => unknown) => {
             // To make the variable available in the catch block
             var _result_;
 
+            console.log("Getting text from selector: " + selector + " (" + property + ")")
             // Try to get the text from the selector
             try {
-                _result_ = await browserInstance.evaluateHandle((selector: string) => {
-                    const element = document.querySelector(selector);
-
-                    return (element as HTMLElement).innerText;
-                }, selector);
+                _result_ = await browserInstance.$eval(selector, (element, property) => { return element[property] }, property);
 
                 // Check if callback is defined and return result
-                return callback(_result_);
+                console.log("Result: " + _result_)
+                if (callback)
+                    return callback(_result_);
+                else
+                    return _result_
             } catch (e) {
                 return "Error";
             }
@@ -213,11 +214,8 @@ async function startBrowser(product_url: string) {
  * @returns The item type (auction or product)
  */
 async function getItemType() {
-    if (await puppeteerHelper().getSelector(".bid-details-time-title")) {
-        return "auction"
-    } else {
-        return "product"
-    }
+    return "auction"
+
 }
 
 /**
@@ -232,20 +230,20 @@ async function getProductInfo(product_url: string) {
     // TODO: Make all selectors use jsselector from chromiun dev tools instead of css selectors (they are more reliable)
     if (await getItemType() == "auction") {
         pageInfo = {
-            sellerName: await puppeteerHelper().getSelector(".seller-alias", async (result) => { return (await result).jsonValue(); }) as string, // Use this format
-            isAuction: await puppeteerHelper().getSelector(".bid-details-bids-title", (result) => { return result.includes("Bud") ? true : false; }),
-            aucutionName: await puppeteerHelper().getSelector("#view-item-main"),
-            auctionEnded: await puppeteerHelper().getSelector(".my-auto > .heading-london", (result) => { return result.includes("Avslutad") ? true : false; }),
+            sellerName: await puppeteerHelper().getSelectorText(".seller-alias", "textContent") as string, // Use this format
+            isAuction: await puppeteerHelper().getSelectorText(".bid-details-bids-title", "innerHTML", (result) => { return result ? true : false }) as boolean,
+            aucutionName: await puppeteerHelper().getSelectorText("#view-item-main"),
+            auctionEnded: await puppeteerHelper().getSelectorText(".my-auto > .heading-london"),
             allBids: await puppeteerHelper().getAllBids(),
             numberOfBids: (await puppeteerHelper().getAllBids()).length,
-            highestBid: await puppeteerHelper().getSelector(".bid-details-amount > span > span", (result) => { return Number(result.replace(/[^0-9.]/g, "")); }),
-            timeLeft: await puppeteerHelper().getSelector("div.flex-md-row:nth-child(2) > div:nth-child(2) > p:nth-child(1)", (result) => { return result != "Avslutad" ? result : 0; }),
+            highestBid: await puppeteerHelper().getSelectorText(".bid-details-amount > span > span", (result) => { return Number(result.replace(/[^0-9.]/g, "")); }),
+            timeLeft: await puppeteerHelper().getSelectorText("div.flex-md-row:nth-child(2) > div:nth-child(2) > p:nth-child(1)", (result) => { return result != "Avslutad" ? result : 0; }),
             latestBid: await puppeteerHelper().getAllBids((result) => { return result[0]; }),
-            buyNowPrice: await puppeteerHelper().getSelector("button.btn-md:nth-child(3)", (result) => { return Number(result.replace(/[^0-9.]/g, "")); }, () => { return 0 }),
-            buyNowAvailable: await puppeteerHelper().getSelector("button.btn-md:nth-child(3)", (result) => { return true; }, () => { return false }),
-            description: await puppeteerHelper().getSelector(".overflow-hidden.text-break.position-relative"),
+            buyNowPrice: await puppeteerHelper().getSelectorText("button.btn-md:nth-child(3)", (result) => { return Number(result.replace(/[^0-9.]/g, "")); }, () => { return 0 }),
+            buyNowAvailable: await puppeteerHelper().getSelectorText("button.btn-md:nth-child(3)", (result) => { return true; }, () => { return false }),
+            description: await puppeteerHelper().getSelectorText(".overflow-hidden.text-break.position-relative"),
             images: await puppeteerHelper().getAllImages(),
-            endTime: await puppeteerHelper().getSelector("#collapsed_auction_details > div > aside > div.separators_sm-separator__lOmzL.pb-md-2.mb-md-2 > section.bid-details.d-flex.flex-md-column.justify-content-between.py-1.pt-md-0.pb-md-2 > div.d-flex.flex-column.flex-md-row.justify-content-between.mb-md-2.text-center > div.mb-1.mb-md-0 > p", (result) => { return result.replace("Avslutas ", ""); })
+            endTime: await puppeteerHelper().getSelectorText("#collapsed_auction_details > div > aside > div.separators_sm-separator__lOmzL.pb-md-2.mb-md-2 > section.bid-details.d-flex.flex-md-column.justify-content-between.py-1.pt-md-0.pb-md-2 > div.d-flex.flex-column.flex-md-row.justify-content-between.mb-md-2.text-center > div.mb-1.mb-md-0 > p", (result) => { return result.replace("Avslutas ", ""); })
         };
     } else {
         pageInfo = {
